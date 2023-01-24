6908b0244e227a035666d48d6fdd5a125d3afe88
xyiliu <89982974+xyiliu@users.noreply.github.com>
2023-01-24 10:55:33 -0500

    add catalog proj

---
+++ locallibrary/catalog/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    # context_object_name = 'book_list'
+    # template_name = '/book_list.html'
+    paginate_by = 2
+
+class BookDetailView(generic.DetailView):
+    # context_object_name = 'book_detail'
+    # template_name = '/book_detail.html'
+    model = Book
